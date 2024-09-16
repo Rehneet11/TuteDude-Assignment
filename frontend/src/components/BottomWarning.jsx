5612a98c615146f4e529e602c30fa8c68f228600
Rehneet11 <rstudy156@gmail.com>
2024-09-16 20:14:15 +0530

    typos solved

--- frontend/src/components/BottomWarning.jsx
+++ frontend/src/components/BottomWarning.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export function BottomWarning({label, buttonText, to}) {
     return <div className="py-2 text-sm flex justify-center">
-      <div>
+      <div className="text-slate-500">
         {label}
       </div>
       <Link className="pointer underline pl-1 cursor-pointer" to={to}>
